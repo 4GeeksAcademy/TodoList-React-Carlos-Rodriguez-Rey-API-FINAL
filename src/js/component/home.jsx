5be35d7a3c15f5764580647cb0ef55dac6e0fd44
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2024-01-12 19:56:30 +0000

    last commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [all, setAll] = useState([]);
 	return (
 		<div className="container">
-			<h1> My To Do List</h1>
+			<h1> My Assignments</h1>
 			<ul>
-				<li><input type="text" placeholder="What do you need?"></input></li>
-				<li>Hola1</li>
-				<li>Hola1</li>
-				<li>Hola1</li>
-				<li>Hola1</li>
-				<li>Hola1</li>
-				<li>Hola1</li>
+				<li><input type="text"
+					onChange={(e) => setInputValue(e.target.value)}
+					value={inputValue}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							setAll(all.concat(inputValue));
+							setInputValue("");
+						}
+					}}
+					placeholder="What do you need?"></input></li>
+
+				{all.map((item, index) => (
+
+					<li> {item} {" "} <i class="fas fa-trash"
+						onClick={() =>
+							setAll(all.filter(
+								(t, currentIndex) =>
+									index != currentIndex
+							)
+							)
+						}></i>
+					</li>
+				))}	
 			</ul>
-		
+			<div> 10 tasks </div>
 		</div>
 	);
 };
