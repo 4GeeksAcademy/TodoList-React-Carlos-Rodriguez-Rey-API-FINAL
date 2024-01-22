6aeeb0b6937a9aee0115ae3eee0cadc82b4e65dd
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2024-01-22 18:58:35 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,43 +1,107 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [all, setAll] = useState([]);
-	return (
-		<div className="container">
-			<h1> My Assignments</h1>
-			<ul>
-				<li><input type="text"
-					onChange={(e) => setInputValue(e.target.value)}
-					value={inputValue}
-					onKeyDown={(e) => {
-						if (e.key === "Enter") {
-							setAll(all.concat(inputValue));
-							setInputValue("");
-						}
-					}}
-					placeholder="What do you need?"></input></li>
+    const [inputValue, setInputValue] = useState("");
+    const [tareas, setTareas] = useState([]);
 
-				{all.map((item, index) => (
+    useEffect(() => {
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/carlosrodriguezR', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(resp => {
+                if (!resp.ok) throw Error('la response no ok')
+                return resp.json()
+            })
+            .then(data => {
+                setTareas(data)
 
-					<li> {item} {" "} <i class="fas fa-trash"
-						onClick={() =>
-							setAll(all.filter(
-								(t, currentIndex) =>
-									index != currentIndex
-							)
-							)
-						}></i>
-					</li>
-				))}	
-			</ul>
-			<div> 10 tasks </div>
-		</div>
-	);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [])
+
+
+    useEffect(() => {
+
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/carlosrodriguezR', {
+                                method: 'PUT',
+                                body: JSON.stringify(tareas),
+                                headers: {
+                                    "Content-Type": "application/json"
+                                }
+                            })
+                                .then(resp => {
+                                    if (!resp.ok) throw Error('la response no ok')
+                                    return resp.json()
+                                })
+                                .then(data => { 
+                                    console.log(data)
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                });
+
+    
+    },[tareas])
+
+    return (
+        <div className="container">
+            <h1> My Assignments</h1>
+            <ul>
+                <li><input type="text"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            let body = tareas.concat([{ "label": inputValue, "done": false}])
+
+                            fetch('https://playground.4geeks.com/apis/fake/todos/user/carlosrodriguezR', {
+                                method: 'PUT',
+                                body: JSON.stringify(body),
+                                headers: {
+                                    "Content-Type": "application/json"
+                                }
+                            })
+                                .then(resp => {
+                                    if (!resp.ok) throw Error('la response no ok')
+                                    return resp.json()
+                                })
+                                .then(data => { 
+                                    setTareas(tareas.concat([{ "label": inputValue, "done": false}]));
+                                    setInputValue("");
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                });
+                        }
+                    }}
+                    placeholder="What do you need?"></input></li>
+
+                {tareas.map((item, index) => (
+
+                    <li> {item.label} {" "} <i class="fas fa-trash"
+                        onClick={() =>
+                            setTareas(tareas.filter(
+                                (t, currentIndex) =>
+                                    index != currentIndex
+                            )
+                            )
+                        }></i>
+                    </li>
+                ))}
+            </ul>
+            <div> 10 tasks </div>
+        </div>
+    );
 };
 
 export default Home;
+
